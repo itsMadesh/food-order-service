b8004ec8338569600214eb7da30bb0c6c3aedb5f
Madeshwaran A <madeshwaran.anandharaj@gmail.com>
2022-10-04 11:06:37 +0530

    Created database connection

--- server.js
+++ server.js
@@ -1,12 +1,18 @@
 const express = require('express');
 const app = express();
+const MongoDBClient = require("./MongoDBClient");
+
+app.use("/api/v1", require("./routes"));
 
 app.get("/", function (req, res) {
     res.send("Hello world");
 });
 
-const port = process.env.PORT || 5000;
-app.listen(port, function (err) {
-    if (err) throw err;
-    console.log(`App running in http://localhost:${port}`)
-});
+(async function () {
+    await MongoDBClient.connect();
+    const port = process.env.PORT || 5000;
+    app.listen(port, async function (err) {
+        if (err) throw err;
+        console.log(`App running in http://localhost:${port}`)
+    });
+})();
